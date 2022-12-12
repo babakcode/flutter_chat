9e71e3bf47e6ce8c0da76b51e76df18fef7abf7f
ebabakg <babakgahremanzadeh@gmail.com>
2022-12-12 16:51:58 +0330

    change server ip

--- lib/constants/app_constants.dart
+++ lib/constants/app_constants.dart
@@ -4,8 +4,8 @@ class AppConstants {
 
   AppConstants._();
   /// sandbox url
-  static String get baseUrl => 'http://192.168.149.220:50001/api';
-  static String get socketBaseUrl => 'http://192.168.149.220:50001/';
+  // static String get baseUrl => 'http://192.168.149.220:50001/api';
+  // static String get socketBaseUrl => 'http://192.168.149.220:50001/';
 
   /// localhost
   // static String get baseUrl => 'http://localhost:50001/api';
@@ -16,8 +16,10 @@ class AppConstants {
   // static String get socketBaseUrl => 'http://192.168.1.173:50001/';
 
   // localhost 3
-  // static String get baseUrl => 'http://217.144.106.223:50001/api';
-  // static String get socketBaseUrl => 'http://217.144.106.223:50001/';
+  static String get baseUrl => 'http://217.144.106.223:50001/api';
+  static String get socketBaseUrl => 'http://217.144.106.223:50001/';
+  // static String get baseUrl => 'https://chat.babakcode.com/api';
+  // static String get socketBaseUrl => 'https://chat.babakcode.com/';
 
   static String get globalEncryptKey => 'jwudownwaodw21ewje2elq2ekwamkwda';
   static Color blueAccent = const Color(0xFF10B2F6);
